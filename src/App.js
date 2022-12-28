5fde6c51c273b979fecd4a14314ecaea59e9830c
Pablo Dias <pablohenrique.mg@gmail.com>
2022-12-28 15:40:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload edited.
+          Edit <code>src/App.js</code> and save to reload edited pablo.
         </p>
         <a
           className="App-link"
